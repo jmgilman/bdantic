8b027d082ed12cc7010a9d02f1d1a1f9c27f2011
Joshua Gilman <joshuagilman@gmail.com>
2022-01-30 08:46:43 -0800

    Overrides json() method on models with more sensible defaults

--- bdantic/models.py
+++ bdantic/models.py
@@ -13,9 +13,14 @@ from beancount.core import (
     position,
 )
 from decimal import Decimal
-from pydantic import BaseModel, Extra, Field
+from pydantic import (
+    BaseModel,
+    Extra,
+    Field,
+)
 from typing import (
     Any,
+    Callable,
     Dict,
     List,
     Literal,
@@ -41,6 +46,33 @@ class Base(BaseModel):
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
+
+    def json(
+        self,
+        *,
+        include: Any = None,
+        exclude: Any = None,
+        by_alias: bool = True,
+        skip_defaults: bool = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = True,
+        encoder: Optional[Callable[[Any], Any]] = None,
+        models_as_dict: bool = True,
+        **dumps_kwargs: Any,
+    ) -> str:
+        return super().json(
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            skip_defaults=skip_defaults,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+            encoder=encoder,
+            models_as_dict=models_as_dict,
+            **dumps_kwargs,
+        )  # type: ignore
 
 
 class BaseDirective(Base):
